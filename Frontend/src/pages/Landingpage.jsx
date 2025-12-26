41e2e98a1d6a38e7cecb8fffdd69a2d3074a18e1
VeerGupta27 <veer27bc067@satiengg.in>
2025-12-26 15:58:14 +0530

    minor changes : added photos, hero section image.

--- Frontend/src/pages/Landingpage.jsx
+++ Frontend/src/pages/Landingpage.jsx
@@ -17,10 +17,11 @@ const LandingPage = () => {
   };
 
   const categories = [
-     { title: "Altars & Temple Setups", slug: "altars", image: "https://pub-43e577891e9c4714928c02b437c487ce.r2.dev/DeitySample.jpeg" },    
-    { title: "Deity Statues", slug: "deities", image: "https://pub-43e577891e9c4714928c02b437c487ce.r2.dev/products/deities/5f44c360-60a5-4170-8df5-eec47ae2b98b.jpeg" },
-    { title: "3D Reviels", slug: "sculptures", image:"" },
-    { title: "Divine Gifts", slug: "custom", image: "" }
+     { title: "Altars & Temple Setups", slug: "altars", image: "https://pub-43e577891e9c4714928c02b437c487ce.r2.dev/products/altars/60fac0cb-b3b1-4c15-bcb9-03e924e4e9d2.jpeg" },    
+    { title: "Deity Statues", slug: "deities", image: "https://pub-43e577891e9c4714928c02b437c487ce.r2.dev/products/deities/06c07679-7777-4367-9d0a-10fb0ebbaa1b.jpeg" },
+    { title: "3D Reviels", slug: "sculptures", image:"https://pub-43e577891e9c4714928c02b437c487ce.r2.dev/products/sculptures/2fb00765-8ff3-4a59-bd23-67dc02a4ef30.jpeg" },
+    { title: "Divine Gifts", slug: "custom", image: "https://pub-43e577891e9c4714928c02b437c487ce.r2.dev/products/custom/5f44c360-60a5-4170-8df5-eec47ae2b98b.jpeg" },
+    { title: "Spiritual Furniture", slug: "furniture", image: "https://pub-43e577891e9c4714928c02b437c487ce.r2.dev/products/furniture/bb81407c-0dcb-4b58-8afd-48f2d617040b.jpeg"}, 
   ];
 
   return (
